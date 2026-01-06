b9962fe6136ef8c105506213d97671d9694e4052
Ratchanon Unnu <ratchanon.u@smartmedtech.co.th>
2026-01-06 17:10:46 +0700

    feat: fluent-bit send log to loki

--- strip_container_name.lua
+++ strip_container_name.lua
@@ -1,4 +1,5 @@
 function strip_name(tag, timestamp, record)
+    record["docker_tag"] = tag
     local name = record["container_name"]
     if name ~= nil then
         if string.sub(name, 1, 1) == "/" then
